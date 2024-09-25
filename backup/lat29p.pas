42e290429208b222e50d68eb834bc1032365776a
Michahide <michaelruswan@gmail.com>
2024-09-25 21:27:57 +0700

    first commit

---
+++ backup/lat29p.pas
@@ -0,0 +1,21 @@
+uses crt;
+var
+nama:string;
+nomor, upah:LongInt;
+jamlembur:byte;
+
+begin clrscr;
+    writeln('==== CV. Kharisma Cahaya Krisnawan Tour & Travel ====');
+    write('Nama: ');
+    readln(nama);
+    write('Nomor Induk Karyawan: ');
+    readln(nomor);
+    write('Jam lembur: ');
+    readln(jamlembur);
+    upah:=3750000+(75000*jamlembur);
+    writeln('Gaji Pokok: Rp 3.750.000,00');
+    writeln('Upah lembur/jam: Rp 75.000,00');
+    write('Upah yang harus dibayar: Rp ',upah,',00.');
+end.
+
+{Michael Mervin Ruswan 202310016 Upah Kerja}
---
+++ backup/lat30p_if.pas
@@ -0,0 +1,47 @@
+uses crt;
+var
+pilihan, jam:byte;
+bayar, uangkembali, totalbayar:longint;
+tanggal, nama:string;
+
+begin clrscr;
+    writeln('==== Sewa Lapangan Futsal ====');
+    writeln('Lapangan yang tersedia: ');
+    writeln('1. Jomblo  Rp 125.000/jam ');
+    writeln('2. Los Dol Rp 145.000/jam ');
+    writeln;
+
+    write('Nama Penyewa: ');
+    readln(nama);
+    write('Tanggal(HH/BB/TTTT): ');
+    readln(tanggal);
+    write('Lama sewa (jam): ');
+    readln(jam);
+    write('Pilih lapangan (Ketikkan angkanya saja): ');
+    readln(pilihan);
+    if (pilihan=1) then
+        begin
+            writeln('Nama Lapangan: Jomblo ');
+            writeln('Harga: Rp 125.000/jam ');
+            totalbayar:=jam*125000;
+        end
+
+        
+    else if (pilihan=2) then
+        begin
+            writeln('Nama Lapangan: Los Dol ');
+            writeln('Harga: Rp 145.000/jam ');
+            totalbayar:=jam*145000;
+        end;
+    
+    
+    writeln('Total Bayar: Rp ',totalbayar);
+    Write('Bayar: Rp ');
+    readln(bayar);
+    uangkembali:=bayar-totalbayar;
+    write('Uang kembali: Rp ', uangkembali);
+
+    readln;
+    
+end.
+{Michael Mervin Ruswan 202310016 Futsal IF}
